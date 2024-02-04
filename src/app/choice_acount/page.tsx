48c9e79119f40a30b065e3b81268b7ac6d68b0ee
moXXcha <chacha.love.sho@icloud.com>
2024-02-05 03:05:19 +0900

    create base

---
+++ src/app/choice_acount/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <p className="mt-10 ml-14 text-2xl font-bold">choice acount</p>
+      <div className="w-96 flex justify-center items-center">
+        <p className="font-bold">user</p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/database.ts
@@ -0,0 +1,4 @@
+export const NAMES = [
+  "chacha",
+  "halyolo",
+];
